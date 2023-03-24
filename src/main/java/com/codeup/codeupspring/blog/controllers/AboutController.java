57308053798ee89c9e8e6a7515937373f092cc4d
RydCri <>
2023-03-24 15:45:27 -0700

    Controller exercises

--- src/main/java/com/codeup/codeupspring/blog/controllers/AboutController.java
+++ src/main/java/com/codeup/codeupspring/blog/controllers/AboutController.java
@@ -2,13 +2,11 @@ package com.codeup.codeupspring.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String landing(){
+public class AboutController {
+    @GetMapping("/about")
+    public String AboutPage(){
         return "about";
     }
 }
