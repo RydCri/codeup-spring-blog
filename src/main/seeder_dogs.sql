6f0615df270ad86198fcea3731d49a4c26dc621e
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-28 16:42:05 -0700

    sql table seeder for posts, products and dogs example exercise

--- src/main/seeder_dogs.sql
+++ src/main/seeder_dogs.sql
@@ -10,3 +10,15 @@ VALUES
     (6, 'Red', 'FL'),
     (8, 'Barney', 'CA'),
     (12, 'Bowser', 'TX');
+
+use springblog_db;
+INSERT INTO products(product_name,cost_in_cents)
+VALUES('Dishwasher',2200),
+      ('Yorkies',1400),
+      ('Tuchonka',800);
+
+use springblog_db;
+INSERT INTO posts(title,body)
+VALUES('I\'m leaving showbusines', 'I just think I\'m not funny anymore.'),
+('you never hear about quicksand anymore','idk it\'s like you just used to hear about it more'),
+('Dogs dont like me', 'I\'m not a bad person\, I like Dogs\, why dont they like me back?');
